11a9c84994f03a2d4dd24f37a28869e4aaa64af7
SofiaMPinho <93188437+SofiaMPinho@users.noreply.github.com>
2022-01-10 23:09:16 +0000

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <unordered_set>
 
 using namespace std;
-int *algo_1(vector<int> sequence, int len);
-int algo_2(vector<int> seq1, int len1, vector<int> seq2, int len2);
+int *num_of_largest_increasing_seqs(vector<int> sequence, int len);
+int size_of_largest_common_seq(vector<int> seq1, int len1, vector<int> seq2, int len2);
 
 int main() {
     int len = 0, type, num = 0, n;
@@ -38,7 +39,7 @@ int main() {
 
     if(type == 1) {
         // int *rtn_val = num_increasing_subs_size_k(sequence, len);
-        int *rtn_val = algo_1(sequence, len);
+        int *rtn_val = num_of_largest_increasing_seqs(sequence, len);
         // printf de t e c, separados por um espaço, onde t é o tamanho da maior
         // subsequencia e c o numero de subsequencias com tamanho t (maximo)
         cout << rtn_val[0] << " " << rtn_val[1] << endl;
@@ -49,20 +50,23 @@ int main() {
         int len2 = 0;
         vector<int> sequence2;
 
+        unordered_set<int> set_seq(sequence.begin(), sequence.end());
         while(cin >> num) {
-            sequence2.push_back(num);
-            len2++;
+            if (set_seq.find(num) != set_seq.end()) {
+                sequence2.push_back(num);
+                len2++;
+            }
         }
 
-        int rtn = algo_2(sequence, len, sequence2, len2);
+        int rtn = size_of_largest_common_seq(sequence, len, sequence2, len2);
         cout << rtn << endl;
     }
     return 0;
 }
 
-int *algo_1(vector<int> sequence, int len){
+int *num_of_largest_increasing_seqs(vector<int> sequence, int len){
     int n_max_size = 0;
-    int *rtn_vals = (int *) malloc(sizeof(int)*2); 
+    int *rtn_vals = new int(2); 
     // length of Longest Increasing Sequence starting at sequence[i]
     vector<int> lengthOfLIS(len, 1);
     // number of Longest Increasing Sequences of length lengthOfLIS[i] starting at sequence[i]
@@ -97,7 +101,7 @@ int *algo_1(vector<int> sequence, int len){
     return rtn_vals;
 }
 
-int algo_2(vector<int> seq1, int len1, vector<int> seq2, int len2)
+int size_of_largest_common_seq(vector<int> seq1, int len1, vector<int> seq2, int len2)
 {
     vector<int> table(len2, 0);
 
@@ -107,13 +111,11 @@ int algo_2(vector<int> seq1, int len1, vector<int> seq2, int len2)
 
         for (int j = 0; j < len2; j++)
         {
-            if (seq1[i] == seq2[j])
-                if (current + 1 > table[j])
-                    table[j] = current + 1;
+            if ((seq1[i] == seq2[j]) && (current + 1 > table[j]))
+                table[j] = current + 1;
 
-            if (seq1[i] > seq2[j])
-                if (table[j] > current)
-                    current = table[j];
+            if ((seq1[i] > seq2[j]) && (table[j] > current))
+                current = table[j];
         }
     }
 
